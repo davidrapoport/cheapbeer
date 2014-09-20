a832c297e22c23b3daa472fb7939e96c37938ca6
David Rapoport <david@david-U56E>
2014-09-20 00:34:58 -0400

    ran rails new

---
+++ backend/cheap_beer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheapBeer::Application.config.secret_key_base = '99bd8d609d954216f9fef974dc96dce6ca1a5dfea6e44e0a932d79921a318a63c5f97e61ba2eca2f5ae4e01ac0fe7165e02f010c0dfe0c133e3b00582326a66b'
---
+++ backend/cheap_beer/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CheapBeer::Application.config.session_store :cookie_store, key: '_cheap_beer_session'
