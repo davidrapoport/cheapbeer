e896432c76ee910ab2b85faebabfb520c2d94542
Marco d'Amore <mdamore9@gmail.com>
2014-09-20 03:23:22 -0400

    Initial Commit

---
+++ ios/CheapBeerNearHere/CheapBeerNearHere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheapBeerNearHere
+//
+//  Created by Marco d'Amore on 9/20/14.
+//  Copyright (c) 2014 Marco d'Amore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/CheapBeerNearHere/CheapBeerNearHere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheapBeerNearHere
+//
+//  Created by Marco d'Amore on 9/20/14.
+//  Copyright (c) 2014 Marco d'Amore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
